a67d11e4e250268835a4d2e81413d30b2e83feba
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-06-09 07:40:29 +0700

    Ver6: Final

--- backend/src/firebase/firebase.service.ts
+++ backend/src/firebase/firebase.service.ts
@@ -1,38 +1,31 @@
 import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import { async } from 'rxjs';
 
 @Injectable()
 export class FirebaseService implements OnModuleInit {
   private readonly logger = new Logger(FirebaseService.name);
   private db: admin.firestore.Firestore;
-  private rtdb: admin.database.Database;
 
   onModuleInit() {
     if (admin.apps.length === 0) {
       this.logger.log('Initializing Firebase Admin fallback...');
-      admin.initializeApp({
-        databaseURL: 'https://monitoring-d6063-default-rtdb.firebaseio.com'
-      });
+      admin.initializeApp();
     }
     this.db = admin.firestore();
-    this.rtdb = admin.database();
-    this.logger.log('Firebase Database services initialized successfully.');
+    this.logger.log('Firestore Database initialized successfully.');
   }
 
   getFirestore(): admin.firestore.Firestore {
     return this.db;
   }
-
-  getDatabase(): admin.database.Database {
-    return this.rtdb;
-  }
-
-  public async verifyIdToken(token: string): Promise<admin.auth.DecodedIdToken> {
-    try {
-      return await admin.auth().verifyIdToken(token);
-    } catch (error) {
-      this.logger.error('Xác thực Firebase Token thất bại:', error);
-      throw error;
-    }
+}
+  async verifyIdToken(token: string) {
+  try {
+    return await admin.auth().verifyIdToken(token);
+  } catch (error) {
+    this.logger.error('Xác thực Firebase Token thất bại:', error);
+    throw error;
   }
 }
+
